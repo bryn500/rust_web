82e9e60d638679123c46dc212d17bc8601686a95
Bryn Thomas <bryn500@outlook.com>
2023-09-01 00:12:16 +0100

    play with enums

--- web-app/tests/test.rs
+++ web-app/tests/test.rs
@@ -7,6 +7,67 @@ pub mod unit_tests {
         num: i32,
     }
 
+    enum TestEnum {
+        Test1(TestData),
+        Test2(i32),
+        Green,
+    }
+
+    #[test]
+    fn test_enum1() {
+        let test_data = TestEnum::Test1(TestData {
+            is: false,
+            name: String::from("test"),
+            num: 1,
+        });
+
+        match_enum(test_data);
+
+        assert_eq!(1, 1);
+    }
+
+    #[test]
+    fn test_enum2() {
+        let test_data = TestEnum::Test2(1);
+
+        match_enum(test_data);
+
+        assert_eq!(1, 1);
+    }
+
+    #[test]
+    fn test_enum3() {
+        let test_data = TestEnum::Green;
+
+        match_enum(test_data);
+
+        assert_eq!(1, 1);
+    }
+
+    fn match_enum(test_data: TestEnum) {
+        match test_data {
+            TestEnum::Test1(val) => match val.name.as_str() {
+                "test" => {
+                    println!("Property value is test");
+                }
+                _ => {
+                    println!("Property value is other struct");
+                }
+            },
+            TestEnum::Test2(val) => match val {
+                1 => {
+                    println!("Property value is 1");
+                }
+                _ => {
+                    println!("Property value is other num");
+                }
+            },
+            TestEnum::Green => {
+                println!("Property value is green");
+            }
+        }
+    }
+
     #[test]
     fn test_rust() {
         let mut tests = Vec::new();
